1e4eebe4df01960da5379e268c89efc3fd4f6dac
matheusmichalski <matheus.michalski11@gmail.com>
2024-08-22 01:49:19 -0300

    Criando API, criando link com banco de dados MongoDB, utilizando o prisma para a manipulação dele e armazenando arquivos com criação de usuários com o método get, put, post e delete.

--- server.js
+++ server.js
@@ -1,11 +1,72 @@
-import express from "express";
+import express, { request, response } from "express";
+import { PrismaClient } from '@prisma/client'
+import e from "express";
 
+const prisma = new PrismaClient()
 const app = express();
+app.use(express.json())
 
-app.get('/usuarios', (request, response) => {
-    response.send(
-        "Ok, deu certo!"
-    )
+
+app.post('/usuarios', async (request, response) => {
+
+    await prisma.user.create({
+        data: {
+            name: request.body.name,
+            age: request.body.age,
+            email: request.body.email
+        }
+    });
+
+    response.status(201).json(request.body)
+});
+
+app.get('/usuarios', async (request, response) => {
+    
+    let users1 =  [];
+
+    if (request.query) {
+        users1 = await prisma.user.findMany({
+            where: {
+                name: request.query.name,
+                age: request.query.age,
+                email: request.query.email,
+            }
+        })
+    } else {
+
+    }
+
+    console.log(request)
+
+    const users = await prisma.user.findMany()
+    response.status(200).json(users)
+});
+
+app.put('/usuarios/:id', async (request, response) => {
+
+    await prisma.user.update({
+        where: {
+            id: request.params.id,
+        },
+        data: {
+            name: request.body.name,
+            age: request.body.age,
+            email: request.body.email
+        }
+    });
+
+    response.status(201).json(request.body)
+});
+
+app.delete(('/usuarios/:id'), async (request, response) => {
+
+    await prisma.user.delete({
+        where: {
+            id: request.params.id
+        }
+    })
+
+    response.status(200).json({ message: "Usuário deletado com sucesso!" })
 });
 
 app.listen(3000)
